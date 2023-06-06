edcab12f09b73a0a44b1158b103534a7e8099b4d
Samuel Gichuhi <gichuhi1sam@gmail.com>
2023-06-06 15:17:33 +0300

    added delete functions to controller class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,66 @@ class ApplicationController < Sinatra::Base
 
     set :default_contetent_type, 'application/json'
 
+    #Artist Requests
     get '/artists' do
        artists = Artist.all
        artists.to_json
     end
+
+
+    get '/artists/:id' do
+        artist = Artist.find(params[:id])
+        artist.to_json
+     end
+     
+     delete '/artists/:id' do
+        artist = Artist.find(params[:id])
+        artist.destroy
+        artist.to_json
+     end
     
+    
+     #Venue Requests
     get '/venues' do
         venues = Venue.all
         venues.to_json
      end
+    
+     get '/venues/:id' do
+        venue = Venue.find(params[:id])
+        venue.to_json
+     end
+
+      
+     delete '/venues/:id' do
+        venue = Venue.find(params[:id])
+        venue.detroy
+        venue.to_json
+     end
      
+
+     #Event Requests
      get '/events' do
          events = Event.all
          events.to_json
      end
 
-    get '/' do
+     get '/events/:id' do
+        event = Event.find(params[:id])
+        event.to_json
+     end
+
+
+     delete '/events/:id' do
+        event = Event.find(params[:id])
+        event.destroy
+        event.to_json
+     end
+     
+     #Extra
+     get '/' do
         "Hello World"
-    end
-  
+     end
+
+
 end
