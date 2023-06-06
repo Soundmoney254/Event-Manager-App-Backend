b19e5207fb735c5e9bb3e62e20cbe2ea7c0393cf
cynthia41 <cynthiaadhiambo41@gmail.com>
2023-06-06 11:53:40 +0300

    second commit

--- db/migrate/20230606064438_create_events.rb
+++ db/migrate/20230606064438_create_events.rb
@@ -1,4 +1,13 @@
 class CreateEvents < ActiveRecord::Migration[7.0]
   def change
+    create_table :events do |t|
+      t.text :name
+      t.integer :venue_id
+      t.datetime :date
+      t.real :price
+      t.text :event_type
+      t.integer :attendees
+      t.integer :artist_id
+    end
   end
 end
