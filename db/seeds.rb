c56202a4c80293b3ed258e95c00684057472ee9c
Samuel <samnmbuguah@gmail.com>
2023-06-08 15:57:55 +0300

    Edited seed data prices

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,12 +23,12 @@ Venue.create(name: "Kenyatta International Conference Centre", price:1500000, ad
 Venue.create(name: "Uhuru Gardens", price:2000000, address:"Uhuru Gardens Rd, Nairobi", capacity:30000, phone_number: "+254712345678" )
 
 
-Event.create(name: "Blankets & Wine", date:'2023-07-11 19:00:00', price:10000 , event_type:"Music", attendees:10294, artist_id:1, venue_id:3)
+Event.create(name: "Blankets & Wine", date:'2023-07-11 19:00:00', price:3000 , event_type:"Music", attendees:10294, artist_id:1, venue_id:3)
 Event.create(name: "Soul'd Out", date:'2023-06-10 19:00:00', price:2000 , event_type:"Music", attendees:420, artist_id:2, venue_id:2)
-Event.create(name: "Bahati Live", date:'2023-07-18 19:00:00', price:15000 , event_type:"Music", attendees:15000, artist_id:1, venue_id:1)
-Event.create(name: "Otile Brown Live", date:'2023-07-25 19:00:00', price:10000 , event_type:"Music", attendees:10000, artist_id:2, venue_id:2)
+Event.create(name: "Bahati Live", date:'2023-07-18 19:00:00', price:1500 , event_type:"Music", attendees:15000, artist_id:1, venue_id:1)
+Event.create(name: "Otile Brown Live", date:'2023-07-25 19:00:00', price:1000 , event_type:"Music", attendees:10000, artist_id:2, venue_id:2)
 Event.create(name: "Nyashinski Live", date:'2023-08-01 19:00:00', price:5000 , event_type:"Music", attendees:5000, artist_id:3, venue_id:3)
-Event.create(name: "Bahati and Otile Brown Live", date:'2023-07-29 19:00:00', price:20000 , event_type:"Music", attendees:20000, artist_id:1, venue_id:1)
+Event.create(name: "Bahati and Otile Brown Live", date:'2023-07-29 19:00:00', price:500 , event_type:"Music", attendees:20000, artist_id:1, venue_id:1)
 Event.create(name: "Nyashinski and Karisoke Live", date:'2023-08-05 19:00:00', price:15000 , event_type:"Music", attendees:15000, artist_id:3, venue_id:2)
-Event.create(name: "Just A Band Live", date:'2023-08-12 19:00:00', price:10000 , event_type:"Music", attendees:10000, artist_id:5, venue_id:3)
+Event.create(name: "Just A Band Live", date:'2023-08-12 19:00:00', price:1000 , event_type:"Music", attendees:10000, artist_id:5, venue_id:3)
 
